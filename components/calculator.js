c0ff7c3c5ada3e5cb140fa926646a7860926484d
Lisa Gaudette <lisa.gaudette@gmail.com>
2023-08-06 09:35:25 -0400

    Implement Calculator Functions

--- components/calculator.js
+++ components/calculator.js
@@ -2,52 +2,185 @@ import {useState} from 'react'
 
 import { NumberButton, Button } from "./widgets";
 
-const Calculator = ({}) => {
-    const [displayValue, setDisplayValue] = useState("")
+import { create, all } from 'mathjs/number'
 
-    function appendToValue(newChar) {
-        setDisplayValue(displayValue + newChar)
+const math = create(all)
+
+const binaryOperators = ["*", "/", "+", "-"]
+
+const Calculator = ({}) => {
+    // A number that is currently being entered
+    const [currentInputValue, setCurrentInputValue] = useState("")
+
+    // The result of a calculation (or retrieved memory)
+    const [result, setResult] = useState("")
+
+    const [expression, setExpression] = useState("")
+
+    const [memory, setMemory] = useState(null)
+
+    const [hasOpenBracket, setHasOpenBracket] = useState(false)
+
+    function getInputValue() { 
+        // Returns the sanitized input value
+        if(currentInputValue !== "") {
+            return currentInputValue.replace(/\.$/)
+        }
+        return ""
     }
 
-    function appendDecimal() {
-        if(!displayValue.includes('.')) {
-            setDisplayValue(displayValue + '.')
+    function getInputValueOrResult() {
+        // Returns the sanitized input value, or, if empty, the previous result.
+        let val = getInputValue()
+        if (val === "") {
+            return result;
         }
     }
 
+    function appendToValue(newChar) {
+        const newValue = currentInputValue + newChar
+        setCurrentInputValue(newValue)
+    }
+
+    function appendDecimal() {
+        if(!currentInputValue.includes('.')) {
+            setCurrentInputValue(currentInputValue + '.')
+        }
+    }
+
+    function getExpressionToEvaluate() {
+        let val;
+        let expr;
+        if(expression.endsWith("=")) {
+            val = result;
+            expr = ""
+        }
+        else {
+            val = getInputValue()
+            expr = expression;
+        }
+        if(val !== "") {
+            if(hasOpenBracket) {
+                expr += `${val})`
+                setHasOpenBracket(false)
+            }
+            else {
+                expr += ` ${val}`
+            }
+        }
+        return expr;
+    }
+
+    function appendOrUpdateOperator(expr, op)
+    {        
+        // If an operator is added and the expression 
+        // already ends with an operator, replace it
+        let trimmed = expression.trimEnd()
+        let lastChar = expr.charAt(trimmed.length-1)
+        if(binaryOperators.includes(lastChar)) {
+            return expr.replace(lastChar, op)
+        }
+        return `${expr} ${op} `
+    }
+
+    function operatorClicked(op) {
+        const expr = getExpressionToEvaluate()
+        setCurrentInputValue("")
+        // Evaluate the results "so far"
+        setResult(math.evaluate(expr))
+        setExpression(appendOrUpdateOperator(expr, op))        
+    }
+
+    function percentClicked() {
+        // Unlike a binary operation, percent should evaluate right away
+        const expr = getExpressionToEvaluate() + "%"
+        setCurrentInputValue("")
+        setResult(math.evaluate(expr))
+        setExpression(expr)
+    }
+
+    function sqrtClicked() {       
+        const expr = getExpressionToEvaluate()
+        setCurrentInputValue("")
+        setResult(math.evaluate(expr))
+        setExpression(`${expr} sqrt(`)
+        setHasOpenBracket(true)
+    }
+
+    function equalClicked() {
+        const val = getInputValue()
+        const expr = getExpressionToEvaluate(val)
+        setCurrentInputValue("")
+        setResult(math.evaluate(expr))
+        setExpression(`${expr} =`)
+    }
+
+    function memoryStore() {
+        setMemory(parseFloat(getInputValueOrResult()))
+    }
+
+    function memoryClear() {
+        setMemory(null)
+    }
+
+    function memoryRecall() {
+        setCurrentInputValue("")
+        setResult(memory)
+    }
+
+    function memoryAdd() {
+        setMemory((memory || 0) + parseFloat(getInputValueOrResult()))
+    }
+
+    function memorySubtract() {
+        setMemory((memory || 0) - parseFloat(getInputValueOrResult()))
+    }
+
+    function clear() {
+        setCurrentInputValue("")
+        setResult("")
+        setExpression("")
+        setHasOpenBracket(false)
+    }
+
+   
     return (
         <div>
             <div className="row">
                 <div className="col">
-                    <input type="text" className="form-control" value={displayValue} readOnly/>
+                    <small className='text-muted'>{expression}</small>
+                    <input type="text" className="form-control text-end" value={currentInputValue || result} readOnly/>
                 </div>
             </div>
             <div className="row">
                 <div className="col">
-                    <Button>M+</Button>
+                    <Button onClick={memoryAdd}>M+</Button>
                 </div>
                 <div className="col">
-                    <Button>M-</Button>
+                    <Button onClick={memorySubtract}>M-</Button>
                 </div>
                 <div className="col">
-                    <Button>MR</Button>
+                    <Button onClick={memoryRecall} disabled={(memory === null)}>MR</Button>
                 </div>
                 <div className="col">
-                    <Button>MC</Button>
+                    <Button onClick={memoryClear}>MC</Button>
+                </div>
+                <div className="col">
+                    <Button onClick={memoryStore}>MS</Button>
                 </div>
             </div>
             <div className="row">
                 <div className="col">
-                    <Button>√</Button>
+                    <Button onClick={sqrtClicked}>√</Button>
                 </div>
                 <div className="col">
-                    <Button>^</Button>
+                    <Button onClick={() => operatorClicked("**")}>^</Button>
                 </div>
                 <div className="col">
-                    <Button>%</Button>
+                    <Button onClick={percentClicked}>%</Button>
                 </div>
                 <div className="col">
-                    <Button>÷</Button>
+                    <Button onClick={() => operatorClicked("/")}>÷</Button>
                 </div>
             </div>
             <div className="row">
@@ -61,7 +194,7 @@ const Calculator = ({}) => {
                     <NumberButton value={9} onClick={appendToValue}/>
                 </div>
                 <div className="col">
-                    <Button>×</Button>
+                    <Button onClick={() => operatorClicked("*")}>×</Button>
                 </div>
             </div>
             <div className="row">
@@ -75,7 +208,7 @@ const Calculator = ({}) => {
                     <NumberButton value={6} onClick={appendToValue}/>
                 </div>
                 <div className="col">
-                    <Button>−</Button>
+                    <Button onClick={() => operatorClicked("-")}>−</Button>
                 </div>
             </div>
             <div className="row">
@@ -89,12 +222,12 @@ const Calculator = ({}) => {
                     <NumberButton value={3} onClick={appendToValue}/>
                 </div>
                 <div className="col">
-                    <Button>+</Button>
+                    <Button onClick={() => operatorClicked("+")}>+</Button>
                 </div>
             </div>
             <div className="row">
                 <div className="col">
-                    <Button onClick={() => setDisplayValue("")}>CE</Button>
+                    <Button onClick={clear}>CE</Button>
                 </div>
                 <div className="col">
                     <NumberButton value={0} onClick={appendToValue}/>
@@ -103,7 +236,7 @@ const Calculator = ({}) => {
                     <NumberButton value={"."} onClick={appendDecimal}/>
                 </div>
                 <div className="col">
-                    <Button>=</Button>
+                    <Button onClick={equalClicked}>=</Button>
                 </div>
             </div>
         </div>
--- components/widgets.js
+++ components/widgets.js
@@ -1,5 +1,5 @@
-const Button = ({ children, onClick }) => (
-    <button type="button" className="btn btn-outline-secondary w-100" onClick={onClick}>{children}</button>);
+const Button = ({ children, onClick, disabled=false }) => (
+    <button type="button" className="btn btn-outline-secondary w-100" onClick={onClick} disabled={disabled}>{children}</button>);
 
 const NumberButton = ({ value, onClick }) => (
     <button type="button" className="btn btn-outline-secondary w-100" onClick={() => onClick(value)}>{value}</button>);
