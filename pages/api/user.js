c43e0135eedbf39f463df3c9ed8379b79c8c6f19
Lisa Gaudette <lisa.gaudette@gmail.com>
2023-08-06 11:51:27 -0400

    Fix bug where users couldn't login

--- pages/api/user.js
+++ pages/api/user.js
@@ -6,7 +6,8 @@ export default async function user(req, res) {
     const session = await getLoginSession(req)
     const user = (session && (await findUser(session))) ?? null
 
-    res.status(200).json({ user })
+    // Don't return sensitive info to client!
+    res.status(200).json({ user: {username: user.username, history: user.history} })
   } catch (error) {
     console.error(error)
     res.status(500).end('Authentication token is invalid, please log in')
